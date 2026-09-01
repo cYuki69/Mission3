3c040edd0133c7d23ab42806017992dd05d959c3
cYuki69 <cyuki1180@gmail.com>
2026-09-01 21:24:03 +0700

    Fix ALLOWED_HOSTS default value

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-change-this')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
